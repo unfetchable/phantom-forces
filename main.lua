17aaf687282aaa2ec175601da927a2aa1d1c8a30
unfetchable <mitu@vite.global>
2022-12-03 18:21:22 -0500

    ok

--- main.lua
+++ main.lua
@@ -1,1 +1,102 @@
-bG9jYWwgdG9yc29fc2l6ZSA9IFZlY3RvcjMubmV3KDUsIDUsIDUpIC0tIGRlZmF1bHQgMiwgMiwgMQpsb2NhbCBzdGVwID0gMS8yNAoKbG9jYWwgc19wbGF5ZXJzID0gZ2FtZTpHZXRTZXJ2aWNlKCdQbGF5ZXJzJykKbG9jYWwgc19yZXBfZmlyc3QgPSBnYW1lOkdldFNlcnZpY2UoJ1JlcGxpY2F0ZWRGaXJzdCcpCgpsb2NhbCBlc3AgPSB7fSBkbwogICAgZXNwLm9iamVjdHMgPSB7fQoKICAgIGxvY2FsIGZfb2JqZWN0ID0gSW5zdGFuY2UubmV3KCdGb2xkZXInKQogICAgZl9vYmplY3QuTmFtZSA9IHRvc3RyaW5nKG1hdGgucmFuZG9tKDExMTExMTExMSwgOTk5OTk5OTk5KSkKICAgIGZfb2JqZWN0LlBhcmVudCA9IHdvcmtzcGFjZQoKICAgIGZ1bmN0aW9uIGVzcC5uZXcocGxheWVyKQogICAgICAgIGVzcC5vYmplY3RzW3BsYXllcl0gPSBJbnN0YW5jZS5uZXcoJ0hpZ2hsaWdodCcpCgogICAgICAgIGxvY2FsIHBfY2hhbSA9IGVzcC5vYmplY3RzW3BsYXllcl0KICAgICAgICBwX2NoYW0uRW5hYmxlZCA9IHRydWUKICAgICAgICBwX2NoYW0uRmlsbENvbG9yID0gQ29sb3IzLm5ldygxLCAwLCAwKQogICAgICAgIHBfY2hhbS5PdXRsaW5lQ29sb3IgPSBDb2xvcjMubmV3KDAuNSwgMCwgMCkKICAgICAgICBwX2NoYW0uRmlsbFRyYW5zcGFyZW5jeSA9IDAuNQogICAgICAgIHBfY2hhbS5PdXRsaW5lVHJhbnNwYXJlbmN5ID0gMC45CiAgICAgICAgcF9jaGFtLlBhcmVudCA9IGZfb2JqZWN0CgogICAgICAgIHJldHVybiBwX2NoYW0KICAgIGVuZAoKICAgIGZvciBfLCBwbGF5ZXIgaW4gcGFpcnMoc19wbGF5ZXJzOkdldFBsYXllcnMoKSkgZG8gZXNwLm5ldyhwbGF5ZXIpIGVuZAoKICAgIHNfcGxheWVycy5QbGF5ZXJBZGRlZDpDb25uZWN0KGZ1bmN0aW9uKHBsYXllcikgZXNwLm5ldyhwbGF5ZXIpIGVuZCkKICAgIHNfcGxheWVycy5QbGF5ZXJSZW1vdmluZzpDb25uZWN0KGZ1bmN0aW9uKHBsYXllcikgZXNwLm9iamVjdHNbcGxheWVyXTpEZXN0cm95KCkgZW5kKQplbmQKCmxvY2FsIGVudGl0eSA9IHt9IGRvCiAgICBsb2NhbCByZXBsaWNhdGlvbiA9IHJlcXVpcmUoc19yZXBfZmlyc3QuQ2xpZW50TW9kdWxlcy5SZXdyaXRlLlJlcGxpY2F0aW9uLlJlcGxpY2F0aW9uSW50ZXJmYWNlKQoKICAgIGZ1bmN0aW9uIGVudGl0eS5nZXQocGxheWVyKQogICAgICAgIGxvY2FsIHBfZW50cnkgPSByZXBsaWNhdGlvbi5nZXRFbnRyeShwbGF5ZXIpCgogICAgICAgIGlmIG5vdCBwX2VudHJ5IHRoZW4gcmV0dXJuIGVuZAogICAgICAgIGlmIG5vdCBwX2VudHJ5Ll90aGlyZFBlcnNvbk9iamVjdCB0aGVuIHJldHVybiBlbmQKICAgICAgICBpZiBub3QgcF9lbnRyeS5fdGhpcmRQZXJzb25PYmplY3Q6Z2V0Q2hhcmFjdGVyTW9kZWwoKSB0aGVuIHJldHVybiBlbmQKICAgICAgICAKICAgICAgICByZXR1cm4gcF9lbnRyeSwgcF9lbnRyeS5fdGhpcmRQZXJzb25PYmplY3QsIHBfZW50cnkuX3RoaXJkUGVyc29uT2JqZWN0OmdldENoYXJhY3Rlck1vZGVsKCkKICAgIGVuZAoKICAgIGxvY2FsIHBsYXllclN0YXR1cyA9IHJlcXVpcmUoc19yZXBfZmlyc3QuQ2xpZW50TW9kdWxlcy5SZXdyaXRlLlBsYXllclN0YXR1cy5QbGF5ZXJTdGF0dXNJbnRlcmZhY2UpCgogICAgZnVuY3Rpb24gZW50aXR5LmZyaWVuZGx5KHBsYXllcikKICAgICAgICBsb2NhbCBwX2VudHJ5ID0gcGxheWVyU3RhdHVzLmdldEVudHJ5KHBsYXllcikKCiAgICAgICAgaWYgbm90IHBfZW50cnkgdGhlbiByZXR1cm4gZW5kCiAgICAgICAgCiAgICAgICAgcmV0dXJuIHBsYXllclN0YXR1cy5pc0ZyaWVuZGx5KHBsYXllcikKICAgIGVuZAplbmQKCmxvY2FsIHRvcnNvX2RlZmF1bHQgPSBWZWN0b3IzLm5ldygyLCAyLCAxKQoKdGFzay5zcGF3bihmdW5jdGlvbigpCiAgICB3aGlsZSB0YXNrLndhaXQoc3RlcCkgZG8KICAgICAgICBmb3IgcGxheWVyLCBjaGFtIGluIHBhaXJzKGVzcC5vYmplY3RzKSBkbwogICAgICAgICAgICBsb2NhbCBwX2VudGl0eSwgcF9vYmplY3QsIHBfbW9kZWwgPSBlbnRpdHkuZ2V0KHBsYXllcikKCiAgICAgICAgICAgIGlmIG5vdCBwX2VudGl0eSB0aGVuCiAgICAgICAgICAgICAgICAgY2hhbS5BZG9ybmVlID0gbmlsCiAgICAgICAgICAgICAgICAgY29udGludWUKICAgICAgICAgICAgZW5kCgogICAgICAgICAgICBpZiBub3QgZW50aXR5LmZyaWVuZGx5KHBsYXllcikgYW5kIHBfbW9kZWwuTmFtZSB+PSAnRGVhZCcgYW5kIHBfbW9kZWwuUGFyZW50IH49IHdvcmtzcGFjZS5JZ25vcmUuRGVhZEJvZHkgdGhlbgogICAgICAgICAgICAgICAgcF9tb2RlbC5Ub3Jzby5TaXplID0gdG9yc29fc2l6ZQoKICAgICAgICAgICAgICAgIGNoYW0uQWRvcm5lZSA9IHBfbW9kZWwKICAgICAgICAgICAgICAgIGNoYW0uRW5hYmxlZCA9IHRydWUKICAgICAgICAgICAgZWxzZQogICAgICAgICAgICAgICAgcF9tb2RlbC5Ub3Jzby5TaXplID0gVmVjdG9yMy5uZXcoMiwgMiwgMSkKCiAgICAgICAgICAgICAgICBjaGFtLkFkb3JuZWUgPSBuaWwKICAgICAgICAgICAgICAgIGNoYW0uRW5hYmxlZCA9IGZhbHNlCiAgICAgICAgICAgIGVuZAogICAgICAgIGVuZAogICAgZW5kCmVuZCkKCnRhc2suc3Bhd24oZnVuY3Rpb24oKQogICAgbG9jYWwgd21fdGV4dCA9IERyYXdpbmcubmV3KCdUZXh0JykKCiAgICB3bV90ZXh0LlRyYW5zcGFyZW5jeSA9IDEKICAgIHdtX3RleHQuVmlzaWJsZSA9IHRydWUKICAgIHdtX3RleHQuWkluZGV4ID0gMQogICAgd21fdGV4dC5DZW50ZXIgPSB0cnVlCiAgICB3bV90ZXh0Lk91dGxpbmUgPSB0cnVlCiAgICB3bV90ZXh0LkNvbG9yID0gQ29sb3IzLm5ldygxLCAxLCAxKQogICAgd21fdGV4dC5PdXRsaW5lQ29sb3IgPSBDb2xvcjMubmV3KDAsIDAsIDApCiAgICB3bV90ZXh0LlBvc2l0aW9uID0gVmVjdG9yMi5uZXcod29ya3NwYWNlLkN1cnJlbnRDYW1lcmEuVmlld3BvcnRTaXplLlgvMiwgMzApCiAgICB3bV90ZXh0LkZvbnQgPSBEcmF3aW5nLkZvbnRzLlBsZXgKICAgIHdtX3RleHQuU2l6ZSA9IDEzCgogICAgd2hpbGUgdGFzay53YWl0KDAuOSkgZG8KICAgICAgICB3bV90ZXh0LlRleHQgPSAnb2ZmbGluZSBlc3AgfCBkZW1vIHwgJy4ub3MuZGF0ZSgiJXggJVgiLCBvcy50aW1lKCkpCiAgICBlbmQKZW5kKQ==
+local torso_size = Vector3.new(5, 5, 5) -- default 2, 2, 1
+local step = 1/24
+
+local s_players = game:GetService('Players')
+local s_rep_first = game:GetService('ReplicatedFirst')
+
+local esp = {} do
+    esp.objects = {}
+
+    local f_object = Instance.new('Folder')
+    f_object.Name = tostring(math.random(111111111, 999999999))
+    f_object.Parent = workspace
+
+    function esp.new(player)
+        esp.objects[player] = Instance.new('Highlight')
+
+        local p_cham = esp.objects[player]
+        p_cham.Enabled = true
+        p_cham.FillColor = Color3.new(1, 0, 0)
+        p_cham.OutlineColor = Color3.new(0.5, 0, 0)
+        p_cham.FillTransparency = 0.5
+        p_cham.OutlineTransparency = 0.9
+        p_cham.Parent = f_object
+
+        return p_cham
+    end
+
+    for _, player in pairs(s_players:GetPlayers()) do esp.new(player) end
+
+    s_players.PlayerAdded:Connect(function(player) esp.new(player) end)
+    s_players.PlayerRemoving:Connect(function(player) esp.objects[player]:Destroy() end)
+end
+
+local entity = {} do
+    local replication = require(s_rep_first.ClientModules.Rewrite.Replication.ReplicationInterface)
+
+    function entity.get(player)
+        local p_entry = replication.getEntry(player)
+
+        if not p_entry then return end
+        if not p_entry._thirdPersonObject then return end
+        if not p_entry._thirdPersonObject:getCharacterModel() then return end
+        
+        return p_entry, p_entry._thirdPersonObject, p_entry._thirdPersonObject:getCharacterModel()
+    end
+
+    local playerStatus = require(s_rep_first.ClientModules.Rewrite.PlayerStatus.PlayerStatusInterface)
+
+    function entity.friendly(player)
+        local p_entry = playerStatus.getEntry(player)
+
+        if not p_entry then return end
+        
+        return playerStatus.isFriendly(player)
+    end
+end
+
+local torso_default = Vector3.new(2, 2, 1)
+
+task.spawn(function()
+    while task.wait(step) do
+        for player, cham in pairs(esp.objects) do
+            local p_entity, p_object, p_model = entity.get(player)
+
+            if not p_entity then
+                 cham.Adornee = nil
+                 continue
+            end
+
+            if not entity.friendly(player) and p_model.Name ~= 'Dead' and p_model.Parent ~= workspace.Ignore.DeadBody then
+                p_model.Torso.Size = torso_size
+
+                cham.Adornee = p_model
+                cham.Enabled = true
+            else
+                p_model.Torso.Size = Vector3.new(2, 2, 1)
+
+                cham.Adornee = nil
+                cham.Enabled = false
+            end
+        end
+    end
+end)
+
+task.spawn(function()
+    local wm_text = Drawing.new('Text')
+
+    wm_text.Transparency = 1
+    wm_text.Visible = true
+    wm_text.ZIndex = 1
+    wm_text.Center = true
+    wm_text.Outline = true
+    wm_text.Color = Color3.new(1, 1, 1)
+    wm_text.OutlineColor = Color3.new(0, 0, 0)
+    wm_text.Position = Vector2.new(workspace.CurrentCamera.ViewportSize.X/2, 30)
+    wm_text.Font = Drawing.Fonts.Plex
+    wm_text.Size = 13
+
+    while task.wait(0.9) do
+        wm_text.Text = 'offline esp | demo | '..os.date("%x %X", os.time())
+    end
+end)
