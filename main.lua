b8b8462f7c025f3fd0c77072fc19ddc611bd2393
unfetchable <mitu@vite.global>
2022-12-05 20:15:12 -0500

    test

--- main.lua
+++ main.lua
@@ -1,85 +1,106 @@
-local torso_size = Vector3.new(5, 5, 5) -- default 2, 2, 1
-local step = 1/24
+-- made by Offline
+-- #pfskiddies
 
-local s_players = game:GetService('Players')
-local s_rep_first = game:GetService('ReplicatedFirst')
+assert(Drawing, "missing dependency: drawing")
+repeat
+	task.wait()
+until game:IsLoaded()
+-- Services
+local players = game:GetService("Players")
+local run_service = game:GetService("RunService")
+-- Localizing
+local local_player = players.LocalPlayer
+local get_children = game.GetChildren
+local find_first_child = game.FindFirstChild
+local new_drawing = Drawing.new
+local camera = workspace.CurrentCamera or game:GetService("Workspace").CurrentCamera
 
-local esp = {} do
-    esp.objects = {}
+-- Setup
+local rootPart = "Head"
+local objects_folder: Folder = game:GetService("Workspace").Players
+local coreGui = game:GetService("CoreGui")
+-- main
+local object_ID = {}
+local eps = {}
 
-    local f_object = Instance.new('Folder')
-    f_object.Name = tostring(math.random(111111111, 999999999))
-    f_object.Parent = workspace
-
-    function esp.new(player)
-        esp.objects[player] = Instance.new('Highlight')
-
-        local p_cham = esp.objects[player]
-        p_cham.Enabled = true
-        p_cham.FillColor = Color3.new(1, 0, 0)
-        p_cham.OutlineColor = Color3.new(0.5, 0, 0)
-        p_cham.FillTransparency = 0.5
-        p_cham.OutlineTransparency = 0.9
-        p_cham.Parent = f_object
-
-        return p_cham
-    end
-
-    for _, player in pairs(s_players:GetPlayers()) do esp.new(player) end
-
-    s_players.PlayerAdded:Connect(function(player) esp.new(player) end)
-    s_players.PlayerRemoving:Connect(function(player) esp.objects[player]:Destroy() end)
+local function Setup()
+	for _, team in pairs(objects_folder:GetChildren()) do
+		for _, object in pairs(get_children(team)) do
+			local id = object:GetDebugId()
+			if not table.find(object_ID, id) then
+				table.insert(object_ID, id)
+				eps[id] = {
+					esp_object = object,
+					text_esp = new_drawing("Text", false),
+					highlight = Instance.new("Highlight", coreGui),
+				}
+				eps[id].text_esp.Center = true
+				eps[id].text_esp.Outline = true
+				eps[id].text_esp.Font = 1
+				eps[id].text_esp.Size = 14
+				eps[id].highlight.Adornee = eps[id].esp_object
+				eps[id].highlight.FillColor = Color3.new(1, 0, 0)
+				eps[id].highlight.OutlineColor = Color3.new(.5, 0, 0)
+				eps[id].highlight.FillTransparency = 0.5
+				eps[id].highlight.OutlineTransparency = 0.9
+			end
+		end
+	end
 end
 
-local entity = {} do
-    local replication = require(s_rep_first.ClientModules.Rewrite.Replication.ReplicationInterface)
+Setup()
+shared.max_distance = math.huge
+shared.object_esp_enabled = true
+run_service.RenderStepped:Connect(function()
+	if not shared.object_esp_enabled then
+		return
+	end
+	for _, value in pairs(eps) do
+		if
+			shared.object_esp_enabled
+			and find_first_child(value.esp_object, rootPart)
+			and value.esp_object.Parent.Name ~= local_player.TeamColor.Name
+		then
+			local name = value.esp_object.Name
+			local vec3_position = find_first_child(value.esp_object, rootPart).Position
+			local screen_position, on_screen = camera:WorldToScreenPoint(vec3_position)
+			local distant_from_character = local_player:DistanceFromCharacter(vec3_position)
+			if on_screen and math.round(distant_from_character) <= shared.max_distance then
+				value.text_esp.Position = Vector2.new(
+					screen_position.X,
+					screen_position.Y + math.clamp(distant_from_character / 10, 10, 30) - 10
+				)
+				value.text_esp.Color = Color3.fromHSV(math.clamp(distant_from_character / 5, 0, 125) / 255, 0.75, 1)
+				value.text_esp.Text = name .. "[" .. math.round(distant_from_character) .. "]"
+				value.text_esp.Size = math.clamp(30 - distant_from_character / 10, 16, 30)
+				value.text_esp.Transparency = math.clamp((500 - distant_from_character) / 200, 0.2, 1)
+				value.text_esp.Visible = false
+				 -- value.highlight.FillColor =
+				 -- 	Color3.fromHSV(math.clamp(distant_from_character / 5, 0, 125) / 255, 0.75, 1)
+				 -- value.highlight.FillTransparency = math.clamp((500 - distant_from_character) / 200, 0.2, 1)
 
-    function entity.get(player)
-        local p_entry = replication.getEntry(player)
-
-        if not p_entry then return end
-        if not p_entry._thirdPersonObject then return end
-        if not p_entry._thirdPersonObject:getCharacterModel() then return end
-        
-        return p_entry, p_entry._thirdPersonObject, p_entry._thirdPersonObject:getCharacterModel()
-    end
-
-    local playerStatus = require(s_rep_first.ClientModules.Rewrite.PlayerStatus.PlayerStatusInterface)
-
-    function entity.friendly(player)
-        local p_entry = playerStatus.getEntry(player)
-
-        if not p_entry then return end
-        
-        return playerStatus.isFriendly(player)
-    end
-end
-
-local torso_default = Vector3.new(2, 2, 1)
-
-task.spawn(function()
-    while task.wait(step) do
-        for player, cham in pairs(esp.objects) do
-            local p_entity, p_object, p_model = entity.get(player)
-
-            if not p_entity then
-                 cham.Adornee = nil
-                 continue
-            end
-
-            if not entity.friendly(player) and p_model.Name ~= 'Dead' and p_model.Parent ~= workspace.Ignore.DeadBody then
-                p_model.Torso.Size = torso_size
-
-                cham.Adornee = p_model
-                cham.Enabled = true
-            else
-                p_model.Torso.Size = Vector3.new(2, 2, 1)
-
-                cham.Adornee = nil
-                cham.Enabled = false
-            end
-        end
-    end
+				value.highlight.Enabled = true
+			else
+				value.text_esp.Visible = false
+				value.highlight.Enabled = false
+			end
+		else
+			value.text_esp.Visible = false
+			value.highlight.Enabled = false
+		end
+	end
+end)
+objects_folder.DescendantAdded:Connect(function()
+	Setup()
+end)
+objects_folder.DescendantRemoving:Connect(function(child)
+	local id = child:GetDebugId()
+	if table.find(object_ID, id) then
+		eps[id].text_esp:Remove()
+		eps[id].highlight:Destroy()
+		eps[id].esp_object = nil
+		eps[id] = nil
+	end
 end)
 
 task.spawn(function()
@@ -97,6 +118,6 @@ task.spawn(function()
     wm_text.Size = 13
 
     while task.wait(0.9) do
-        wm_text.Text = 'offline esp | demo | '..os.date("%x %X", os.time())
+        wm_text.Text = 'offline esp | patch fix | '..os.date("%x %X", os.time())
     end
 end)
