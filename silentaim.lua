febfe64e4ddf4b8ef7f3c62496fa5c1e5b52939f
unfetchable <mitu@vite.global>
2022-12-06 16:25:45 -0500

    z

---
+++ silentaim.lua
@@ -0,0 +1,89 @@
+-- phantom forces, silent aim
+-- by Harmer#0123, working 12/6/2022
+
+-- variables
+local players = game:GetService("Players");
+local replicated = game:GetService("ReplicatedFirst");
+local localplayer = players.LocalPlayer;
+local camera = workspace.CurrentCamera;
+
+-- modules
+local particle;
+local replication;
+local physics, solve;
+
+for _, module in next, getloadedmodules() do
+    if module.Name == "particle" then
+        particle = require(module);
+    elseif module.Name == "ReplicationInterface" then
+        replication = require(module);
+    elseif module.Name == "physics" then
+        physics = require(module);
+        solve = debug.getupvalue(physics.timehit, 2);
+    end
+end
+
+-- functions
+local function isVisible(position, ignore)
+    return #camera:GetPartsObscuringTarget({ position }, ignore) == 0;
+end
+
+local function getClosest(dir, origin, ignore)
+    local _angle = fov or 180;
+    local _position, _entry;
+
+    replication.operateOnAllEntries(function(player, entry)
+        local tpObject = entry and entry._thirdPersonObject;
+        local character = tpObject and tpObject._character;
+        if character and player.Team ~= localplayer.Team then
+            local position = character[targetedPart == "Random" and
+                (math.random() < (headChance or 0.5) and "Head" or "Torso") or
+                (targetedPart or "Head")].Position;
+
+            if not (visibleCheck and not isVisible(position, ignore)) then
+                local dot = dir.Unit:Dot((position - origin).Unit);
+                local angle = 180 - (dot + 1) * 90;
+                if angle < _angle then
+                    _angle = angle;
+                    _position = position;
+                    _entry = entry;
+                end
+            end
+        end
+    end);
+
+    return _position, _entry;
+end
+
+local function trajectory(dir, velocity, accel, speed)
+    local r1, r2, r3, r4 = solve(
+        accel:Dot(accel) * 0.25,
+        accel:Dot(velocity),
+        accel:Dot(dir) + velocity:Dot(velocity) - speed^2,
+        dir:Dot(velocity) * 2,
+        dir:Dot(dir));
+
+    local time = (r1>0 and r1) or (r2>0 and r2) or (r3>0 and r3) or r4;
+    local bullet = 0.5*accel*time + dir/time + velocity;
+    return bullet, time; -- kill me
+end
+
+-- hooks
+local old;
+old = hookfunction(particle.new, function(args)
+    if debug.info(2, "n") == "fireRound" then
+        local position, entry = getClosest(args.velocity, args.visualorigin, args.physicsignore);
+        if position and entry then
+            local index = table.find(debug.getstack(2), args.velocity);
+
+            args.velocity = trajectory(
+                position - args.visualorigin,
+                entry._velspring._p0,
+                -args.acceleration,
+                args.velocity.Magnitude);
+
+            debug.setstack(2, index, args.velocity);
+        end
+    end
+    return old(args);
+end);
